6b47c8621eef55759356cfe808caebb23fd59ac1
Brian Midei <brian.midei@gmail.com>
2020-06-01 23:37:22 -0400

    Added team visibility commands

---
+++ commands/getAllTeams.js
@@ -0,0 +1,43 @@
+const db = require('../db_accessor.js')
+
+module.exports = {
+  name: 'teams',
+  description: 'Command to get a list of all teams in the server or tournament',
+  async execute(message, args) {
+    if (args.length != 1) {
+      message.channel.send('You must specify whether you are requesting the teams for the server or the tournament');
+    }
+
+    if (args[0] === 'server') {
+      const teams = await db.getAllTeamsInServer(message.guild.id);
+      message.channel.send(formatTeamsForOutput(teams));
+      return;
+    }
+
+    if (args[0] === 'tournament') {
+
+      const tournament = await db.getLatestTournament(message.guild.id);
+      const teams = await db.getTeamsInTournament(message.guild.id, tournament.id);
+      console.log(teams);
+      message.channel.send(formatTeamsForOutput(teams));
+      return;
+    }
+  }
+}
+
+function formatTeamsForOutput(teams) {
+  // TODO formatting needs work. Look into discord embed objects
+  // TODO bring this into a util function. It'll get used in a few places
+  let out = ''
+  teams.forEach(team => {
+    out += 'Team Name: ' + team.teamName;
+    out += '\n\nPlayers:'
+    team.players.forEach((elem, idx) => {
+      if (idx != 0) {out += '\n';};
+      out += '\n\tActivisionID: ' + elem.activisionId;
+      out += '\n\tGamertag: ' + elem.gamertag;
+    })
+    out += '\n';
+  })
+  return out;
+}
